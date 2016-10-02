12aea49a4408e809e3d7291a2268f509f6b2e62b
brandonxiang <1542453460@qq.com>
2016-10-02 16:38:37 +0800

    plugin init

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 L.VtGeojsonHeat = L.GridLayer.extend({
 
     initialize: function (map, options) {
-        var featurecollection = {
+        this.featurecollection = {
             type: 'FeatureCollection',
             features: []
         }
-        this.heat = L.heatmap().addTo(map)
-		L.setOptions(this, options);
-	},
+        this.heat = L.heatLayer([]).addTo(map)
+        L.setOptions(this, options);
+    },
 
-    createTile: function(coords, done){
-          var tile = [coords.x,coord.y,coord.z]
-          vt2geojson(tile)
+    createTile: function (coords, done) {
+        var tile = [coords.x, coords.y, coords.z]
+        this.vt2geojson(tile)
+        return L.DomUtil.create("div");
+    },
 
-     },
+    vt2geojson: function (tile) {
+        var that = this;
+        var layers = 'poi_label'
+        var tiles = "tilejson+http://localhost:8000/tilejson.json"
+        vtgeojson(tiles, {
+            tiles: [tile],
+            layers: layers ? layers.split(',') : undefined
+        })
+        .on('data', function (data) {
+            that.featurecollection.features.push(data)
+            var coord = [data.geometry.coordinates[1], data.geometry.coordinates[0]];
+            that.heat.addLatLng(coord);
+        })
+    }
+});
 
-     vt2geojson: function (tile) {
-            var layers = 'poi_label'
-            var tiles = "tilejson+http://localhost:8000/tilejson.json"
-            vtgeojson(tiles, {
-                tiles: [tile],
-                layers: layers ? layers.split(',') : undefined
-            })
-            .on('data', function(data) {
-                this.featurecollection.features.push(data)
-                heat.addLatLng(data.geometry.coordinates);
-            })
-        }
- });
+L.vtGeojsonHeat = function (map, options) {
+    return new L.VtGeojsonHeat(map, options);
+}
